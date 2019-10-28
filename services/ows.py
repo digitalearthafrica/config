9f100a89b68106ac17884767dc0fcae8bbf77459
Tom Butler <tom.butler@ga.gov.au>
2019-10-28 22:07:56 +0000

    add wofs all-time summary

--- services/ows.py
+++ services/ows.py
@@ -2059,16 +2059,320 @@ layer_cfg = [
                 the understanding of surface water across Africa.  The layers available are: 
                 the count of wet observations; the percentage of wet observations over time. 
 
-            This layer contains Wet Count: how many times water was detected in observations that were clear. No 
-            clear observations of water causes an area to appear transparent, 1-50 total clear observations of water 
-            correlate with red and yellow colours, 100 clear observations of water correlate with green, 
-            200 clear observations of water correlates with light blue, 300 clear observations of water correlates to 
-            deep blue and 400 and over observations of clear water correlate to purple. 
+                This layer contains Wet Count: how many times water was detected in observations that were clear. No 
+                clear observations of water causes an area to appear transparent, 1-50 total clear observations of water 
+                correlate with red and yellow colours, 100 clear observations of water correlate with green, 
+                200 clear observations of water correlates with light blue, 300 clear observations of water correlates to 
+                deep blue and 400 and over observations of clear water correlate to purple. 
 
-            As no confidence filtering is applied to this product, it is affected by noise where mis-classifications 
-            have occurred in the WOfS water classifications, and hence can be difficult to interpret on its own. The 
-            confidence layer and filtered summary are contained in the Water Observations from Space Statistics 
-            Filtered Summary product, which provide a noise-reduced view of the water summary.""",
+                As no confidence filtering is applied to this product, it is affected by noise where mis-classifications 
+                have occurred in the WOfS water classifications, and hence can be difficult to interpret on its own. The 
+                confidence layer and filtered summary are contained in the Water Observations from Space Statistics 
+                Filtered Summary product, which provide a noise-reduced view of the water summary.""",
+                "min_zoom_factor": 0,
+                # The fill-colour of the indicative polygons when zoomed out.
+                # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
+                "zoomed_out_fill_colour": [150, 180, 200, 160],
+                # Time Zone.  In hours added to UTC (maybe negative)
+                # Used for rounding off scene times to a date.
+                # Central Africa Timezone (UTC+2).
+                "time_zone": 2,
+                # Extent mask function
+                # Determines what portions of dataset is potentially meaningful data.
+                "extent_mask_func": lambda data, band: (data[band] != data[band].attrs['nodata']),
+                # Flags listed here are ignored in GetFeatureInfo requests.
+                # (defaults to empty list)
+                "ignore_info_flags": [],
+                "legend": {
+                    # "url": ""
+                    "styles": ["water_observations"]
+                },
+                "wcs_default_bands": ["count_wet"],
+                "styles": [
+                    {
+                        "name": "water_observations",
+                        "title": "Count Wet",
+                        "abstract": "WOfS summary showing the count of water observations",
+                        "needed_bands": ["count_wet"],
+                        "color_ramp": [
+                            {
+                                "value": 0,
+                                "color": "#666666",
+                                "alpha": 0
+                            },
+                            {
+                                # purely for legend display
+                                # we should not get fractional
+                                # values in this styles
+                                "value": 0.2,
+                                "color": "#990000",
+                                "alpha": 1
+                            },
+                            {
+                                "value": 2,
+                                "color": "#990000"
+                            },
+                            {
+                                "value": 2,
+                                "color": "#990000"
+                            },
+                            {
+                                "value": 4,
+                                "color": "#E38400"
+                            },
+                            {
+                                "value": 6,
+                                "color": "#E3DF00"
+                            },
+                            {
+                                "value": 8,
+                                "color": "#00E32D"
+                            },
+                            {
+                                "value": 10,
+                                "color": "#00E3C8"
+                            },
+                            {
+                                "value": 12,
+                                "color": "#0097E3"
+                            },
+                            {
+                                "value": 14,
+                                "color": "#005FE3"
+                            },
+                            {
+                                "value": 16,
+                                "color": "#000FE3"
+                            },
+                            {
+                                "value": 18,
+                                "color": "#000EA9"
+                            },
+                            {
+                                "value": 20,
+                                "color": "#5700E3",
+                                "legend": {
+                                    "prefix": ">"
+                                }
+                            }
+                        ],
+                        "legend": {
+                            "radix_point": 0,
+                            "scale_by": 1,
+                            "major_ticks": 10
+                        }
+                    }
+                ],
+                # Default style (if request does not specify style)
+                # MUST be defined in the styles list above.
+
+                # (Looks like Terria assumes this is the first style in the list, but this is
+                #  not required by the standard.)
+                "default_style": "water_observations",
+            },
+                        {
+                # Included as a keyword  for the layer
+                "label": "WOfS C2 Statistics",
+                # Included as a keyword  for the layer
+                "type": "Water Summary",
+                # Included as a keyword  for the layer
+                "variant": "30m",
+                # The WMS name for the layer
+                "name": "wofs_2_summary_statistics",
+                # The Datacube name for the associated data product
+                "product_name": "ga_ls8c_wofs_2_summary",
+                # required to support eo3 metadata format
+                "time_resolution": "year",
+                "abstract": """Water Observations from Space (WOfS) Statistics is a set of statistical summaries of 
+                the WOfS product which combines WOfS observations into summary products that help the understanding 
+                of surface water across Africa. The water detected for each location is summed through time and then 
+                compared to the number of clear observations of that location. 
+                The result is a percentage value of the number of times water 
+                was observed at the location. The layers available are: the count of 
+                wet observations; the percentage of wet observations over time (water summary). 
+
+                This layer contains the Water Summary: the percentage of clear observations which were detected as 
+                wet (ie. the ratio of wet to clear as a percentage). No clear observations of water causes an area to 
+                appear transparent, few clear observations of water correlate with red and yellow colours, 
+                deep blue and purple correspond to an area being wet through 90%-100% of clear observations. 
+
+                As no confidence filtering is applied to this product, it is affected by noise where 
+                mis-classifications have occurred in the WOfS water classifications, and hence can be difficult to 
+                interpret on its own. The confidence layer and filtered summary are contained in the Water 
+                Observations from Space Statistics Filtered Summary product, which provide a noise-reduced view of 
+                the water summary.""",
+                "min_zoom_factor": 0,
+                # The fill-colour of the indicative polygons when zoomed out.
+                # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
+                "zoomed_out_fill_colour": [150, 180, 200, 160],
+                # Time Zone.  In hours added to UTC (maybe negative)
+                # Used for rounding off scene times to a date.
+                # Central Africa Timezone (UTC+2).
+                "time_zone": 2,
+                # Extent mask function
+                # Determines what portions of dataset is potentially meaningful data.
+                "extent_mask_func": lambda data, band: (data[band] != data[band].attrs['nodata']),
+                # Flags listed here are ignored in GetFeatureInfo requests.
+                # (defaults to empty list)
+                "ignore_info_flags": [],
+                "legend": {
+                    # "url": ""
+                    "styles": ["WOfS_frequency", "WOfS_frequency_blues_transparent"]
+                },
+                "wcs_default_bands": ["frequency"],
+                "styles": [
+                    {
+                        "name": "WOfS_frequency",
+                        "title": " Water Summary",
+                        "abstract": "WOfS summary showing the frequency of Wetness",
+                        "needed_bands": ["frequency"],
+                        "color_ramp": [
+                            {
+                                "value": 0.0,
+                                "color": "#000000",
+                                "alpha": 0.0
+                            },
+                            {
+                                "value": 0.02,
+                                "color": "#000000",
+                                "alpha": 0.0
+                            },
+                            {
+                                "value": 0.05,
+                                "color": "#8e0101",
+                                "alpha": 0.25
+                            },
+                            {
+                                "value": 0.1,
+                                "color": "#cf2200",
+                                "alpha": 0.75
+                            },
+                            {
+                                "value": 0.2,
+                                "color": "#e38400"
+                            },
+                            {
+                                "value": 0.3,
+                                "color": "#e3df00"
+                            },
+                            {
+                                "value": 0.4,
+                                "color": "#a6e300"
+                            },
+                            {
+                                "value": 0.5,
+                                "color": "#00e32d"
+                            },
+                            {
+                                "value": 0.6,
+                                "color": "#00e3c8"
+                            },
+                            {
+                                "value": 0.7,
+                                "color": "#0097e3"
+                            },
+                            {
+                                "value": 0.8,
+                                "color": "#005fe3"
+                            },
+                            {
+                                "value": 0.9,
+                                "color": "#000fe3"
+                            },
+                            {
+                                "value": 1.0,
+                                "color": "#5700e3"
+                            }
+                        ],
+                        "legend": {
+                            "url": "https://data.dea.ga.gov.au/WOfS/filtered_summary/v2.1.0/wofs_full_summary_legend"
+                                   ".png",
+                        }
+                    },
+                    {
+                        "name": "WOfS_frequency_blues_transparent",
+                        "title": "Water Summary (Blue)",
+                        "abstract": "WOfS summary showing the frequency of Wetness",
+                        "needed_bands": ["frequency"],
+                        "color_ramp": [
+                            {
+                                "value": 0.0,
+                                "color": "#ffffff",
+                                "alpha": 0.0,
+                            },
+                            {
+                                "value": 0.001,
+                                "color": "#d5fef9",
+                                "alpha": 0.0,
+                            },
+                            {
+                                "value": 0.02,
+                                "color": "#d5fef9",
+                            },
+                            {
+                                "value": 0.2,
+                                "color": "#71e3ff"
+                            },
+                            {
+                                "value": 0.4,
+                                "color": "#01ccff"
+                            },
+                            {
+                                "value": 0.6,
+                                "color": "#0178ff"
+                            },
+                            {
+                                "value": 0.8,
+                                "color": "#2701ff"
+                            },
+                            {
+                                "value": 1.0,
+                                "color": "#5700e3"
+                            }
+                        ],
+                        "legend": {
+                            "units": "%",
+                            "radix_point": 0,
+                            "scale_by": 100.0,
+                            "major_ticks": 0.1
+                        }
+                    },
+                ],
+                # Default style (if request does not specify style)
+                # MUST be defined in the styles list above.
+
+                # (Looks like Terria assumes this is the first style in the list, but this is
+                #  not required by the standard.)
+                "default_style": "WOfS_frequency",
+            },
+            {
+                # Included as a keyword  for the layer
+                "label": "WOfS C2 Statistics",
+                # Included as a keyword  for the layer
+                "type": "Wet Count",
+                # Included as a keyword  for the layer
+                "variant": "30m",
+                # The WMS name for the layer
+                "name": "wofs_2_summary_wet",
+                # The Datacube name for the associated data product
+                "product_name": "ga_ls8c_wofs_2_summary",
+                # required to support eo3 metadata format
+                "time_resolution": "year",
+                "abstract": """Water Observations from Space (WOfS) Statistics is a set of statistical summaries of 
+                the WOfS product that combines the many years of WOfS observations into summary products which help 
+                the understanding of surface water across Africa.  The layers available are: 
+                the count of wet observations; the percentage of wet observations over time. 
+
+                This layer contains Wet Count: how many times water was detected in observations that were clear. No 
+                clear observations of water causes an area to appear transparent, 1-50 total clear observations of water 
+                correlate with red and yellow colours, 100 clear observations of water correlate with green, 
+                200 clear observations of water correlates with light blue, 300 clear observations of water correlates to 
+                deep blue and 400 and over observations of clear water correlate to purple. 
+
+                As no confidence filtering is applied to this product, it is affected by noise where mis-classifications 
+                have occurred in the WOfS water classifications, and hence can be difficult to interpret on its own. The 
+                confidence layer and filtered summary are contained in the Water Observations from Space Statistics 
+                Filtered Summary product, which provide a noise-reduced view of the water summary.""",
                 "min_zoom_factor": 0,
                 # The fill-colour of the indicative polygons when zoomed out.
                 # Triplets (rgb) or quadruplets (rgba) of integers 0-255.
