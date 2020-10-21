0a9782f08f53d3ee188ce2304bdcc9d136cc1937
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-10-21 04:45:46 +0000

    add bands

--- services/ows_cfg.py
+++ services/ows_cfg.py
@@ -94,6 +94,19 @@ bands_sentinel = {
     "SCL": ["mask", "qa"]
 }
 
+bands_s2_gm = {
+    "B02": ["band_02", "blue"],
+    "B03": ["band_03", "green"],
+    "B04": ["band_04", "red"],
+    "B05": ["band_05", "red_edge_1"],
+    "B06": ["band_06", "red_edge_2"],
+    "B07": ["band_07", "red_edge_3"],
+    "B08": ["band_08", "nir", "nir_1"],
+    "B8A": ["band_8a", "nir_narrow", "nir_2"],
+    "B11": ["band_11", "swir_1", "swir_16"],
+    "B12": ["band_12", "swir_2", "swir_22"],
+}
+
 bands_ls8c = {
     "red": [],
     "green": [],
