37663c265aa8c31105c41b9166004e8e7a0aaca8
Fang Yuan <fangfy@users.noreply.github.com>
2021-02-10 13:57:07 +1100

    Update ows_cfg.py
    Fixed Sentinel-2 data availability to be from 2017.

--- services/ows_cfg.py
+++ services/ows_cfg.py
@@ -1930,7 +1930,7 @@ Surface reflectance is the fraction of incoming solar radiation that is reflecte
 
 DE Africa provides Sentinel 2 Level-2A surface reflectance data from European Commission's Copernicus Programme. Sentinel-2 is an Earth observation mission that systematically acquires optical imagery at up to 10 m spatial resolution. The mission is based on a constellation of two identical satellites in the same orbit, 180° apart for optimal coverage and data delivery. Together, they cover all Earth's land surfaces, large islands, inland and coastal waters every 3-5 days. Each of the Sentinel-2 satellites carries a wide swath high-resolution multispectral imager with 13 spectral bands.
 
-This product has a temporal coverage of 2015 to current and is updated as new images are acquired. Images in different spectral bands are provided at spatial resolutions of 10, 20 or 60 m. The surface reflectance values are scaled to be between 0 and 10,000.
+This product has a temporal coverage of 2017 to current and is updated as new images are acquired. Images in different spectral bands are provided at spatial resolutions of 10, 20 or 60 m. The surface reflectance values are scaled to be between 0 and 10,000.
 
 Sentinel-2 Level-2A data are provided by the European Space Agency (ESA).  Data prior to 2017 are processed from Level-1C to Level-2A with ESA's Sen2Cor software by Sinergise. All images are converted to Cloud Optimised GeoTIFF format by Element 84, Inc.
 
