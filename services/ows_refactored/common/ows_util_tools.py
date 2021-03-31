f69d3de583f7978e383b82acdff803122b4afebb
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-03-31 06:15:02 +0000

    fix lint

--- services/ows_refactored/common/ows_util_tools.py
+++ services/ows_refactored/common/ows_util_tools.py
@@ -4,6 +4,7 @@ import numpy  # pylint: disable=import-error
 def mask_by_nan(data, band):
     return ~numpy.isnan(data[band])
 
+
 def mask_by_emad_nan(data, band, band_mapper=None):
     if band_mapper:
         emad = band_mapper("EMAD")
