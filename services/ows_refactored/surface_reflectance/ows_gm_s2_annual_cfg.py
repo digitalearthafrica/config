da5c79df64eb2f71320f539e398812c63b9ff7f1
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-04-13 02:43:17 +0000

    fix linting

--- services/ows_refactored/surface_reflectance/ows_gm_s2_annual_cfg.py
+++ services/ows_refactored/surface_reflectance/ows_gm_s2_annual_cfg.py
@@ -6,30 +6,30 @@ layers = {
     "title": "Annual",
     "abstract": """Annual surface reflectance""",
     "layers": [
-            {
+        {
             "title": "Surface Reflectance Annual GeoMAD Sentinel-2",
             "name": "gm_s2_annual",
             "abstract": """
-        Individual remote sensing images can be affected by noisy data, such as clouds, cloud shadows, and haze. To produce cleaner images that can be compared more easily across time, we can create 'summary' images or 'composites' that combine multiple images into one image to reveal the median or 'typical' appearance of the landscape for a certain time period.
+Individual remote sensing images can be affected by noisy data, such as clouds, cloud shadows, and haze. To produce cleaner images that can be compared more easily across time, we can create 'summary' images or 'composites' that combine multiple images into one image to reveal the median or 'typical' appearance of the landscape for a certain time period.
 
-        One approach is to create a geomedian. A geomedian is based on a high-dimensional statistic called the 'geometric median' (Small 1990), which effectively trades a temporal stack of poor-quality observations for a single high-quality pixel composite with reduced spatial noise (Roberts et al. 2017). In contrast to a standard median, a geomedian maintains the relationship between spectral bands. This allows further analysis on the composite images, just as we would on the original satellite images (e.g. by allowing the calculation of common band indices like NDVI). An annual geomedian image is calculated from the surface reflectance values drawn from a calendar year.
+One approach is to create a geomedian. A geomedian is based on a high-dimensional statistic called the 'geometric median' (Small 1990), which effectively trades a temporal stack of poor-quality observations for a single high-quality pixel composite with reduced spatial noise (Roberts et al. 2017). In contrast to a standard median, a geomedian maintains the relationship between spectral bands. This allows further analysis on the composite images, just as we would on the original satellite images (e.g. by allowing the calculation of common band indices like NDVI). An annual geomedian image is calculated from the surface reflectance values drawn from a calendar year.
 
-        In addition, surface reflectance varabilities within the same time period can be measured to support characterization of the land surfaces. The median absolute deviation (MAD) is a robust measure (resilient to outliers) of the variability within a dataset. For multi-spectral Earth observation, deviation can be measured against the geomedian using a number of distance metrics.  Three of these metrics are adopted to highlight different types of changes in the landscape:
-        - Euclidean distance (EMAD), which is more sensitive to changes in target brightness.
-        - Cosine (spectral) distance (SMAD), which is more sensitive to changes in target spectral response.
-        - Bray Curtis dissimilarity (BCMAD), which is more sensitive to the distribution of the observation values through time.
+In addition, surface reflectance varabilities within the same time period can be measured to support characterization of the land surfaces. The median absolute deviation (MAD) is a robust measure (resilient to outliers) of the variability within a dataset. For multi-spectral Earth observation, deviation can be measured against the geomedian using a number of distance metrics.  Three of these metrics are adopted to highlight different types of changes in the landscape:
+- Euclidean distance (EMAD), which is more sensitive to changes in target brightness.
+- Cosine (spectral) distance (SMAD), which is more sensitive to changes in target spectral response.
+- Bray Curtis dissimilarity (BCMAD), which is more sensitive to the distribution of the observation values through time.
 
-        More techincal information about the GeoMAD product can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/GeoMAD_specs.html)
+More techincal information about the GeoMAD product can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/GeoMAD_specs.html)
 
-        This product has a spatial resolution of 10 m and is available annually for 2017 to 2020.
+This product has a spatial resolution of 10 m and is available annually for 2017 to 2020.
 
-        It is derived from Surface Reflectance Sentinel-2 data. This product contains modified Copernicus Sentinel data 2017-2020.
+It is derived from Surface Reflectance Sentinel-2 data. This product contains modified Copernicus Sentinel data 2017-2020.
 
-        Annual geomedian images enable easy visual and algorithmic interpretation, e.g. understanding urban expansion, at annual intervals. They are also useful for characterising permanent landscape features such as woody vegetation. The MADs can be used on their own or together with geomedian to gain insights about the land surface, e.g. for land cover classificiation and for change detection from year to year.
+Annual geomedian images enable easy visual and algorithmic interpretation, e.g. understanding urban expansion, at annual intervals. They are also useful for characterising permanent landscape features such as woody vegetation. The MADs can be used on their own or together with geomedian to gain insights about the land surface, e.g. for land cover classificiation and for change detection from year to year.
 
-        For more information on the algorithm, see https://doi.org/10.1109/TGRS.2017.2723896 and https://doi.org/10.1109/IGARSS.2018.8518312
+For more information on the algorithm, see https://doi.org/10.1109/TGRS.2017.2723896 and https://doi.org/10.1109/IGARSS.2018.8518312
 
-        """,
+""",
             "product_name": "gm_s2_annual",
             # Low product name
             #
