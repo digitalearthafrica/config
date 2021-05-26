906b27efb74769a6a4c89904c03db63d17fd07a2
eefaye <eefaye.chong@gmail.com>
2021-05-26 06:37:19 +0000

    Fixed indents.

--- services/ows_refactored/surface_reflectance/ows_lsc2_sr_cfg.py
+++ services/ows_refactored/surface_reflectance/ows_lsc2_sr_cfg.py
@@ -60,30 +60,30 @@ For more information on Landsat products, see https://www.usgs.gov/core-science-
 
 This product is accessible through OGC Web Service (https://ows.digitalearth.africa/), for analysis in DE Africa Sandbox JupyterLab (https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/wiki) and for direct download from AWS S3 (https://data.digitalearth.africa/).
 """,
-            "product_name": "ls7_sr",
-            "bands": bands_ls7_sr,
-            "resource_limits": reslim_landsat,
-            "image_processing": {
-                "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
-                "always_fetch_bands": [],
-                "manual_merge": False,  # True
-                "apply_solar_corrections": False,
-            },
-            "wcs": {
-                "native_crs": "EPSG:4326",
-                "native_resolution": [30.0, -30.0],
-                "default_bands": ["red", "green", "blue"],
-            },
-            "styling": {
-                "default_style": "simple_rgb",
-                "styles": styles_lsc2_sr_list,
-            },
-        }
+    "product_name": "ls7_sr",
+    "bands": bands_ls7_sr,
+    "resource_limits": reslim_landsat,
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+        "always_fetch_bands": [],
+        "manual_merge": False,  # True
+        "apply_solar_corrections": False,
+    },
+    "wcs": {
+        "native_crs": "EPSG:4326",
+        "native_resolution": [30.0, -30.0],
+        "default_bands": ["red", "green", "blue"],
+    },
+    "styling": {
+        "default_style": "simple_rgb",
+        "styles": styles_lsc2_sr_list,
+    },
+}
 
 layer_ls5 = {
-            "title": "Surface Reflectance Landsat 5 (USGS Collection 2)",
-            "name": "ls5_sr",
-            "abstract": """
+    "title": "Surface Reflectance Landsat 5 (USGS Collection 2)",
+    "name": "ls5_sr",
+    "abstract": """
 Surface reflectance is the fraction of incoming solar radiation that is reflected from Earth's surface. Variations in satellite measured radiance due to atmospheric properties have been corrected for, so images acquired over the same area at different times are comparable and can be used readily to detect changes on Earth’s surface.
 
 DE Africa provides access to Landsat Collection 2 Level-2 Surface Reflectance products over Africa. USGS Landsat Collection 2 offers improved processing, geometric accuracy, and radiometric calibration compared to previous Collection 1 products. The Level-2 products are endorsed by the Committee on Earth Observation Satellites (CEOS) to be Analysis Ready Data for Land (CARD4L)-compliant.
