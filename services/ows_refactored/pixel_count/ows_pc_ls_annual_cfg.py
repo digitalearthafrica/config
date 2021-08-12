c5e1a8b7f93fba8de6d28aa1a54c5302972ce2d8
phaesler <paul.haesler@data61.csiro.au>
2021-08-12 17:27:38 +1000

    Fix typos.

--- services/ows_refactored/pixel_count/ows_pc_ls_annual_cfg.py
+++ services/ows_refactored/pixel_count/ows_pc_ls_annual_cfg.py
@@ -190,7 +190,7 @@ Annual number of observations and number of clear observations. This product is 
     "bands": bands_pc_ls8_annual,
     "resource_limits": reslim_srtm,
     "native_crs": "EPSG:6933",
-    "native_resolution": [30.0, -30,0],
+    "native_resolution": [30.0, -30.0],
     "image_processing": {
         "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
         "always_fetch_bands": [],
