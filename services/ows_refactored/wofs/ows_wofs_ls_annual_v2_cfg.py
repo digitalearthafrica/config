6da5d8e6f7eb8f553bb5322ddb9e764fb3c20b5c
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-08-25 09:55:13 +1000

    refactor wofs-ls-annual-summary ows config - create a style cfg separately

--- services/ows_refactored/wofs/ows_wofs_ls_annual_v2_cfg.py
+++ services/ows_refactored/wofs/ows_wofs_ls_annual_v2_cfg.py
@@ -5,8 +5,8 @@ from ows_refactored.wofs.style_wofs_ls_annual_summary_cfg import (
     style_wofs_summary_annual_clear, style_wofs_summary_annual_wet)
 
 layer = {
-    "title": "Water Observations from Space Annual Summary",
-    "name": "wofs_ls_summary_annual",
+    "title": "Water Observations from Space Annual Summary - v2",
+    "name": "wofs_ls_summary_annual_v2",
     "abstract": """
 Annual water summary is one of the statistical summaries of the Water Observation from Space (WOfS) product that shows what percentage of clear observations were detected as wet (ie. the ration of wet to clear as a percentage) from each calendar year.
 
@@ -22,7 +22,7 @@ For more information on the algorithm, see https://doi.org/10.1016/j.rse.2015.11
 
 This product is accessible through OGC Web Service (https://ows.digitalearth.africa/), for analysis in DE Africa Sandbox JupyterLab (https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/wiki) and for direct download from AWS S3 (https://data.digitalearth.africa/).
 """,
-    "product_name": "wofs_ls_summary_annual",
+    "product_name": "wofs_ls_summary_annual_v2",
     "time_resolution": "year",
     "bands": bands_wofs_summary,
     "resource_limits": reslim_wofs,
