66b02d65eafeb1ecc88949604ee5dde74b8ef95b
phaesler <paul.haesler@data61.csiro.au>
2021-08-25 14:11:25 +1000

    Apply smart resource limits.

--- services/ows_refactored/common/ows_reslim_cfg.py
+++ services/ows_refactored/common/ows_reslim_cfg.py
@@ -1,4 +1,16 @@
 # reslim
+
+reslim_smart = {
+    "wms": {
+        "zoomed_out_fill_colour": [150, 180, 200, 160],
+        "min_zoom_level": 6.0,
+        # "max_datasets": 16, # Defaults to no dataset limit
+    },
+    "wcs": {
+        "max_datasets": 64
+    }, 
+}
+
 reslim_landsat = {
     "wms": {
         "zoomed_out_fill_colour": [150, 180, 200, 160],
