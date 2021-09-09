adba4d199c9c9d3efeb096545842f2db249c4743
eefaye <45861242+eefaye@users.noreply.github.com>
2021-09-09 15:36:11 +1000

    Added suggestions. (#275)
    * Added suggestions.
    
    * Reinstated measurement names.

--- services/ows_refactored/agriculture/ows_crop_mask_cfg.py
+++ services/ows_refactored/agriculture/ows_crop_mask_cfg.py
@@ -2,36 +2,10 @@ from ows_refactored.common.ows_reslim_cfg import reslim_alos_palsar
 
 bands_crop_mask = {"mask": [], "prob": [], "filtered": []}
 
-
-# old style
-style_crop_mask_magma = {
-    "name": "magma",
-    "title": "PROB",
-    "abstract": "Prob band Magma",
-    "needed_bands": ["prob"],
-    "index_function": {
-        "function": "datacube_ows.band_utils.single_band",
-        "mapped_bands": True,
-        "kwargs": {
-            "band": "prob",
-        },
-    },
-    "mpl_ramp": "magma",
-    "range": [50, 100],
-    "legend": {
-        "begin": "50",
-        "end": "100",
-        "ticks_every": "10",
-        "tick_labels": {
-            "50": {"prefix": "<"},
-        },
-    },
-}
-
 style_crop_mask_prob = {
     "name": "prob",
-    "title": "Crop probability",
-    "abstract": "Crop probability",
+    "title": "Probability of cropping",
+    "abstract": "Probability of cropping",
     "needed_bands": ["prob"],
     "index_function": {
         "function": "datacube_ows.band_utils.single_band",
@@ -101,12 +75,12 @@ style_crop_mask_prob = {
 
 style_crop_mask_green = {
     "name": "green",
-    "title": "Crop area",
+    "title": "Cropped land",
     "abstract": "Classified as crop",
     "value_map": {
         "mask": [
             {
-                "title": "Crop area",
+                "title": "Cropped land",
                 "color": "#00FF00",  # (Or #FFFF00)
                 "values": [1],
             }
@@ -116,12 +90,12 @@ style_crop_mask_green = {
 
 style_crop_mask_yellow = {
     "name": "yellow",
-    "title": "Crop area",
+    "title": "Cropped land",
     "abstract": "Classified as crop",
     "value_map": {
         "mask": [
             {
-                "title": "Crop area",
+                "title": "Cropped land",
                 "color": "#FFFF00",  # (Or #FFFF00)
                 "values": [1],
             }
@@ -131,12 +105,12 @@ style_crop_mask_yellow = {
 
 style_crop_mask_filtered_green = {
     "name": "filtered_green",
-    "title": "Crop area (object-filtered)",
+    "title": "Cropped land (object-filtered)",
     "abstract": "Classified as crop and filtered",
     "value_map": {
         "filtered": [
             {
-                "title": "Crop area",
+                "title": "Cropped land (object-filtered)",
                 "color": "#00FF00",  # (Or #FFFF00)
                 "values": [1],
             }
@@ -146,12 +120,12 @@ style_crop_mask_filtered_green = {
 
 style_crop_mask_filtered_yellow = {
     "name": "filtered_yellow",
-    "title": "Crop area (object-filtered)",
+    "title": "Cropped land (object-filtered)",
     "abstract": "Classified as crop and filtered",
     "value_map": {
         "filtered": [
             {
-                "title": "Crop area",
+                "title": "Cropped land (object-filtered)",
                 "color": "#FFFF00",  # (Or #FFFF00)
                 "values": [1],
             }
@@ -166,13 +140,13 @@ style_crop_mask_reversed = {
     "value_map": {
         "mask": [
             {
-                "title": "Crop area",
+                "title": "Cropped land",
                 "color": "#CCCCCC",  # (Or #FFFF00)
                 "values": [1],
                 "mask": True,  # transparent
             },
             {
-                "title": "No Crop",
+                "title": "No crop",
                 "color": "#CCCCCC",  # (Or #FFFF00)
                 "values": [0],
             },
@@ -191,16 +165,16 @@ The cropland extent map for Africa is currently only avilable for Eastern and We
             "title": "Cropland extent 2019 - Eastern Africa",
             "name": "crop_mask_eastern",
             "abstract": """
-Digital Earth Africa's cropland extent map for Eastern Africa shows the location of croplands in the countries of Tanzania, Kenya, Uganda, Ethiopia, Rwanda, and Burundi. Cropland is defined as: "a piece of land of minimum 0.04 ha (a single 10m x 10m pixel) that is sowed/planted and harvest-able at least once within the 12 months after the sowing/planting date." This definition will exclude non-planted grazing lands and perennial crops which can be difficult for satellite imagery to differentiate from natural vegetation.
+Digital Earth Africa's cropland extent map for Eastern Africa (2019) shows the estimated location of croplands in the countries of Tanzania, Kenya, Uganda, Ethiopia, Rwanda, and Burundi for the period January to December 2019. Cropland is defined as: "a piece of land of minimum 0.01 ha (a single 10m x 10m pixel) that is sowed/planted and harvest-able at least once within the 12 months after the sowing/planting date." This definition will exclude non-planted grazing lands and perennial crops which can be difficult for satellite imagery to differentiate from natural vegetation.
 
 This provisional cropland extent map has a resolution of 10m, and was built using Copernicus Sentinel-2 satellite images from 2019. The cropland extent map was produced using extensive training data from Eastern Africa, coupled with a Random Forest machine learning model. For a detailed exploration of the methods used to produce the cropland extent map, read the Jupyter Notebooks in DE Africa’s crop-mask GitHub repository.
 
 An independent validation dataset suggests this service has an overall accuracy of 90.3 %. The algorithm tends to report more omission errors (labelling actual crops as non-crops) than commission errors (labelling non-crops as crops). Where commission errors occur they tend to be focussed around wetlands and seasonal grasslands (e.g. in the Serengeti) which spectrally resemble some kinds of cropping.
 
-The crop mask contains three bands:
-- mask: This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is a pixel-based cropland extent map, meaning the map displays the raw output of the pixel-based Random Forest classification.
-- prob: This band displays the prediction probabilities for the 'crop' class. As this service used a random forest classifier, the prediction probabilities refer to the percentage of trees that voted for the random forest classification. For example, if the model had 200 decision trees in the random forest, and 150 of the trees voted 'crop', the prediction probability is 150 / 200 x 100 = 75 %. Thresholding this band at 50 % will produce a map identical to mask.
-- filtered: This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is an object-based cropland extent map where the mask band has filtered using an image segmentation algorithm. During this process, segments smaller than 1 Ha (100 10m x 10m pixels) are merged with neighbouring segments, resulting in a map where the smallest classified region is 1 Ha is size. The filtered dataset is provided as small commission errors are removed by this process, and the 'salt and pepper' effect typical of classifying pixels is diminished.
+The crop mask contains three measurements:
+- mask ("Cropped land"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is a pixel-based cropland extent map, meaning the map displays the raw output of the pixel-based Random Forest classification.
+- prob ("Probability of cropping"): This band displays the prediction probabilities for the 'crop' class during 2019. As this service used a random forest classifier, the prediction probabilities refer to the percentage of trees that voted for the random forest classification. For example, if the model had 200 decision trees in the random forest, and 150 of the trees voted 'crop', the prediction probability is 150 / 200 x 100 = 75 %. Thresholding this band at 50 % will produce a map identical to mask.
+- filtered ("Cropped land (object-filtered)"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is an object-based cropland extent map where the mask band has filtered using an image segmentation algorithm. During this process, segments smaller than 1 Ha (100 10m x 10m pixels) are merged with neighbouring segments, resulting in a map where the smallest classified region is 1 Ha in size. The filtered dataset is provided as small commission errors are removed by this process, and the 'salt and pepper' effect typical of classifying pixels is diminished.
 
 More technical information about the cropland extent service can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/Cropland_extent_specs.html)
 
@@ -235,16 +209,16 @@ This service is accessible through OGC Web Service (https://ows.digitalearth.afr
             "title": "Cropland extent 2019 - Western Africa",
             "name": "crop_mask_western",
             "abstract": """
-Digital Earth Africa's cropland extent map for Western Africa shows the location of croplands in the countries of Nigeria, Benin, Togo, Ghana, Cote d’Ivoire, Liberia, Sierra Leone, Guinea, and Guinea-Bissau. Cropland is defined as: "a piece of land of minimum 0.04 ha (a single 10m x 10m pixel) that is sowed/planted and harvest-able at least once within the 12 months after the sowing/planting date." This definition will exclude non-planted grazing lands and perennial crops which can be difficult for satellite imagery to differentiate from natural vegetation.
+Digital Earth Africa's cropland extent map for Western Africa (2019) shows the estimated location of croplands in the countries of Nigeria, Benin, Togo, Ghana, Cote d’Ivoire, Liberia, Sierra Leone, Guinea, and Guinea-Bissau for the period January to December 2019. Cropland is defined as: "a piece of land of minimum 0.01 ha (a single 10m x 10m pixel) that is sowed/planted and harvest-able at least once within the 12 months after the sowing/planting date." This definition will exclude non-planted grazing lands and perennial crops which can be difficult for satellite imagery to differentiate from natural vegetation.
 
 This provisional cropland extent map has a resolution of 10m, and was built using Copernicus Sentinel-2 satellite images from 2019. The cropland extent map was produced using extensive training data from Western Africa, coupled with a Random Forest machine learning model. For a detailed exploration of the methods used to produce the cropland extent map, read the Jupyter Notebooks in DE Africa’s crop-mask GitHub repository.
 
 An independent validation dataset suggests this service has an overall accuracy of 83.6 %. The algorithm tends to report more omission errors (labelling actual crops as non-crops) than commission errors (labelling non-crops as crops). Where commission errors occur they tend to be focussed around wetlands and seasonal grasslands which spectrally resemble some kinds of cropping.
 
-The crop mask contains three bands:
-- mask: This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is a pixel-based cropland extent map, meaning the map displays the raw output of the pixel-based Random Forest classification.
-- prob: This band displays the prediction probabilities for the 'crop' class. As this service used a random forest classifier, the prediction probabilities refer to the percentage of trees that voted for the random forest classification. For example, if the model had 200 decision trees in the random forest, and 150 of the trees voted 'crop', the prediction probability is 150 / 200 x 100 = 75 %. Thresholding this band at 50 % will produce a map identical to mask.
-- filtered: This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is an object-based cropland extent map where the mask band has filtered using an image segmentation algorithm. During this process, segments smaller than 1 Ha (100 10m x 10m pixels) are merged with neighbouring segments, resulting in a map where the smallest classified region is 1 Ha is size. The filtered dataset is provided as small commission errors are removed by this process, and the 'salt and pepper' effect typical of classifying pixels is diminished.
+The crop mask contains three measurements:
+- mask ("Cropped land"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is a pixel-based cropland extent map, meaning the map displays the raw output of the pixel-based Random Forest classification.
+- prob ("Probability of cropping"): This band displays the prediction probabilities for the 'crop' class during 2019. As this service used a random forest classifier, the prediction probabilities refer to the percentage of trees that voted for the random forest classification. For example, if the model had 200 decision trees in the random forest, and 150 of the trees voted 'crop', the prediction probability is 150 / 200 x 100 = 75 %. Thresholding this band at 50 % will produce a map identical to mask.
+- filtered ("Cropped land (object-filtered)"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is an object-based cropland extent map where the mask band has filtered using an image segmentation algorithm. During this process, segments smaller than 1 Ha (100 10m x 10m pixels) are merged with neighbouring segments, resulting in a map where the smallest classified region is 1 Ha in size. The filtered dataset is provided as small commission errors are removed by this process, and the 'salt and pepper' effect typical of classifying pixels is diminished.
 
 More technical information about the cropland extent service can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/Cropland_extent_specs.html)
 
