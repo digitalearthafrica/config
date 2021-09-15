bfd905d03c00a30a4b12fc776759abfa158b9ab2
eefaye <eefaye.chong@gmail.com>
2021-09-15 02:10:52 +0000

    Added smart_reslim6

--- services/ows_refactored/surface_reflectance/ows_gm_s2_semiannual_cfg.py
+++ services/ows_refactored/surface_reflectance/ows_gm_s2_semiannual_cfg.py
@@ -1,4 +1,4 @@
-from ows_refactored.common.ows_reslim_cfg import reslim_smart9s2, reslim_zoom9
+from ows_refactored.common.ows_reslim_cfg import reslim_smart9s2, reslim_zoom9, reslim_smart6
 from ows_refactored.surface_reflectance.band_sr_cfg import bands_s2_gm
 from ows_refactored.surface_reflectance.style_sr_cfg import styles_gm_list
 
@@ -91,7 +91,7 @@ For more information on the algorithm, see https://doi.org/10.1109/TGRS.2017.272
     # "low_res_product_name": "gm_s2_annual_lowres",
     "bands": bands_s2_gm,
     "dynamic": False,
-    "resource_limits": reslim_smart9s2,
+    "resource_limits": reslim_smart6,
     "time_resolution": "month",
     "image_processing": {
         "extent_mask_func": "ows_refactored.common.ows_util_tools.mask_by_emad_nan",
