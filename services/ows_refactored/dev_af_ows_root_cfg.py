158fc1cfb47188ae554e53955bb482822be22499
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2021-09-20 18:43:55 +1000

    add wofs landsat summary alltime layer to dev ows

--- services/ows_refactored/dev_af_ows_root_cfg.py
+++ services/ows_refactored/dev_af_ows_root_cfg.py
@@ -242,6 +242,16 @@ ows_cfg = {
                                 }
                             ]
                         },
+                        {
+                            "title": "All-time water",
+                            "abstract": """Alltime water""",
+                            "layers": [
+                                {
+                                    "include": "ows_refactored.wofs.ows_wofs_ls_alltime_cfg.layer",
+                                    "type": "python",
+                                }
+                            ]
+                        },
                     ]
                 },
                 {
