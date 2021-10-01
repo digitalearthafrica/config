c23b17f6e4d7a4ab6c6a34f5199c2c3568ad6b13
eefaye <eefaye.chong@gmail.com>
2021-10-01 05:14:57 +0000

    Keep new wofs config in dev.

--- services/ows_refactored/wofs/ows_wofs_ls_alltime_cfg.py
+++ services/ows_refactored/wofs/ows_wofs_ls_alltime_cfg.py
@@ -7,7 +7,52 @@ from ows_refactored.wofs.style_wofs_ls_legacy import (
     legacy_style_wofs_summary_alltime_frequency_blue,
     legacy_style_wofs_summary_alltime_wet)
 
+# live in prod
 layer = {
+    "title": "Water Observations from Space All-Time Summary",
+    "name": "wofs_ls_summary_alltime",
+    "abstract": """
+All time water summary is one of the statistical summaries of the Water Observation from Space (WOfS) product that shows what percentage of clear observations were detected as wet (ie. the ration of wet to clear as a percentage) over time.
+
+This product has a spatial resolution of 30 m and a temporal coverage of 1980s to last calender year.
+
+It is derived from Landsat Collection 2 surface reflectance product.
+
+All time water summary can be used to understand water availability and flooding risk in a historical context.
+
+WOfS shows surface water on the day and time that satellite passed overhead, which might be before, during or after a flood peak. Given the time between satellite passes (approximately once every 16 days) it is unlikely that the satellite will capture the maximum extent of any given flood. Instead, it aims to provide large scale, regional information on surface water.
+
+For more information on the algorithm, see https://doi.org/10.1016/j.rse.2015.11.003
+
+This product is accessible through OGC Web Service (https://ows.digitalearth.africa/), for analysis in DE Africa Sandbox JupyterLab (https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/wiki) and for direct download from AWS S3 (https://data.digitalearth.africa/).
+""",
+    "product_name": "wofs_ls_summary_alltime",
+    "bands": bands_wofs_summary,
+    "time_resolution": "year",
+    "resource_limits": reslim_wofs,
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+        "always_fetch_bands": [],
+        "manual_merge": False,
+    },
+    "native_crs": "EPSG:6933",
+    "native_resolution": [30.0, -30.0],
+    "wcs": {
+        "default_bands": ["frequency"],
+    },
+    "styling": {
+        "default_style": "legacy_wofs_summary_alltime_frequency",
+        "styles": [
+            legacy_style_wofs_summary_alltime_frequency,
+            legacy_style_wofs_summary_alltime_frequency_blue,
+            legacy_style_wofs_summary_alltime_wet,
+            legacy_style_wofs_summary_alltime_clear,
+        ],
+    },
+}
+
+# for testing in dev 
+dev_layer = {
     "title": "Water Observations from Space All-Time Summary",
     "name": "wofs_ls_summary_alltime",
     "abstract": """
