c091581bec6f26da005e2426b38c6ef8b2598dba
eefaye <eefaye.chong@gmail.com>
2021-10-15 05:42:46 +0000

    Indents.

--- services/ows_refactored/common/ows_reslim_cfg.py
+++ services/ows_refactored/common/ows_reslim_cfg.py
@@ -5,19 +5,19 @@ dataset_cache_rules = [
     {
        "min_datasets": 5,
        "max_age": 60 * 60 * 24,
-   },
-   {
+    },
+    {
        "min_datasets": 9,
        "max_age": 60 * 60 * 24 * 7,
-   },
-   {
+    },
+    {
        "min_datasets": 17,
        "max_age": 60 * 60 * 24 * 30,
-   },
-   {
+    },
+    {
        "min_datasets": 65,
        "max_age": 60 * 60 * 24 * 120,
-   },
+    },
 ]
 
 reslim_wms_min_zoom_15 = {
