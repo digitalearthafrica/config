7d68fab3b887e31c915b4d733ac21abcbb706926
eefaye <eefaye.chong@gmail.com>
2021-10-15 06:01:32 +0000

    More indents.

--- services/ows_refactored/common/ows_reslim_cfg.py
+++ services/ows_refactored/common/ows_reslim_cfg.py
@@ -3,20 +3,20 @@
 # Defines cache control on GetMap requests
 dataset_cache_rules = [
     {
-       "min_datasets": 5,
-       "max_age": 60 * 60 * 24,
+        "min_datasets": 5,
+        "max_age": 60 * 60 * 24,
     },
     {
-       "min_datasets": 9,
-       "max_age": 60 * 60 * 24 * 7,
+        "min_datasets": 9,
+        "max_age": 60 * 60 * 24 * 7,
     },
     {
-       "min_datasets": 17,
-       "max_age": 60 * 60 * 24 * 30,
+        "min_datasets": 17,
+        "max_age": 60 * 60 * 24 * 30,
     },
     {
-       "min_datasets": 65,
-       "max_age": 60 * 60 * 24 * 120,
+        "min_datasets": 65,
+        "max_age": 60 * 60 * 24 * 120,
     },
 ]
 
