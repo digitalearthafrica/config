7b3448d88f79cb85852c419bfb6b106ec2e3d47f
eefaye <eefaye.chong@gmail.com>
2021-11-09 04:01:41 +0000

    Updated CHIRPS descriptions.

--- services/ows_refactored/meteorology/ows_rainfall.py
+++ services/ows_refactored/meteorology/ows_rainfall.py
@@ -1,10 +1,9 @@
 from ows_refactored.common.ows_reslim_cfg import reslim_smart5
 
-
-style_rainfall = {
-    "name": "rainfall",
-    "title": "Chirps Monthly Rainfall",
-    "abstract": "I see the rain, down in Africa",
+style_rainfall_monthly = {
+    "name": "rainfall_monthly",
+    "title": "Monthly rainfall - CHIRPS",
+    "abstract": "Monthly rainfall from the Climate Hazards Group InfraRed Precipitation with Station data (CHIRPS) 35+ year quasi-global rainfall data set.",
     "needed_bands": ["rainfall"],
     "index_function": {
         "function": "datacube_ows.band_utils.single_band",
@@ -50,7 +49,9 @@ layer = {
     "abstract": """
 Climate Hazards Group InfraRed Precipitation with Station data (CHIRPS) is a 35+ year quasi-global rainfall data set.
 
-Spanning 50°S-50°N (and all longitudes) and ranging from 1981 to near-present, CHIRPS incorporates our in-house climatology, CHPclim, 0.05° resolution satellite imagery, and in-situ station data to create gridded rainfall time series for trend analysis and seasonal drought monitoring.
+Spanning 50°S-50°N (and all longitudes) and ranging from 1981 to near-present, CHIRPS incorporates climatology (CHPclim model), 0.05° resolution satellite imagery, and in-situ station data to create gridded rainfall time series for trend analysis and seasonal drought monitoring.
+
+CHIRPS is a USGS and Climate Hazards Centre initiative with funding from USAID, NASA, and NOAA. Find out more on the CHIRPS website (https://www.chc.ucsb.edu/data/chirps). 
 """,
     "product_name": "rainfall_chirps_monthly",
     "time_resolution": "month",
@@ -69,7 +70,7 @@ Spanning 50°S-50°N (and all longitudes) and ranging from 1981 to near-present,
         -0.050000000745058060,
     ],
     "styling": {
-        "default_style": "rainfall",
-        "styles": [style_rainfall],
+        "default_style": "rainfall_monthly",
+        "styles": [style_rainfall_monthly],
     },
 }
