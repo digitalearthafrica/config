a0db03e9a9222c37a5664a2d6111cf470b4e39cb
Fang Yuan <fang.yuan@digitalearthafrica.org>
2021-11-18 09:36:03 +0000

    Updated gmw description

--- services/ows_refactored/vegetation/ows_gmw_cfg.py
+++ services/ows_refactored/vegetation/ows_gmw_cfg.py
@@ -20,9 +20,9 @@ layer = {
     "title": "Global Mangrove Watch",
     "name": "gmw",
     "abstract": """
-The GMW aims to provide geospatial information about mangrove extent and changes to the Ramsar Convention, national wetland practitioners, decision makers and NGOs. It is part of the Ramsar Science and Technical Review Panel (STRP) work plan for 2016-2018 and a Pilot Project to the Ramsar Global Wetlands Observation System (GWOS), which is implemented under the GEO-Wetlands Initiative. The primary objective of the GMW has been to provide countries lacking a national mangrove monitoring system with first cut mangrove extent and change maps, to help safeguard against further mangrove forest loss and degradation.
+The Global Mangrove Watch (GMW) aims to provide geospatial information about mangrove extent and changes to the Ramsar Convention, national wetland practitioners, decision makers and NGOs. It is part of the Ramsar Science and Technical Review Panel (STRP) work plan for 2016-2018 and a Pilot Project to the Ramsar Global Wetlands Observation System (GWOS), which is implemented under the GEO-Wetlands Initiative. The primary objective of the GMW has been to provide countries lacking a national mangrove monitoring system with first cut mangrove extent and change maps, to help safeguard against further mangrove forest loss and degradation.
 
-The GMW has generated a global baseline map of mangroves for 2010 using ALOS PALSAR and Landsat (optical) data, and changes from this baseline for six epochs between 1996 and 2016 derived from JERS-1 SAR, ALOS PALSAR and ALOS-2 PALSAR-2. Annual maps are planned from 2018 and onwards.
+DE Africa provides access to GMW mangrove extent data over Africa. This dataset is available for the years 1996, 2007, 2008, 2009, 2010, 2015, and 2016. The extents have been rasterized to a spatial resolution of 0.0002 deg (~22 m).
 
 For more information, see https://data.unep-wcmc.org/datasets/45.
 """,
