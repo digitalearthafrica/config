2f95e99a6d6d41c262f1df8e06fb77c37cd378d2
Ee Faye C <45861242+eefaye@users.noreply.github.com>
2022-05-06 14:40:27 +1000

    Add SRTM derivs to dev OWS (#415)
    * Add SRTM derivs to dev OWS
    
    Co-authored-by: Fang Yuan <fangfy@users.noreply.github.com>

--- services/ows_refactored/dev_af_ows_root_cfg.py
+++ services/ows_refactored/dev_af_ows_root_cfg.py
@@ -278,8 +278,8 @@ ows_cfg = {
                     ],
                 },
                 {
-                    "title": "Elevation",
-                    "abstract": """Digital elevation model""",
+                    "title": "Elevation and topography",
+                    "abstract": """Digital elevation models and topographic indices""",
                     "layers": [
                         {
                             "include": "ows_refactored.elevation.ows_elevation_cfg.layer_srtm",
@@ -291,6 +291,10 @@ ows_cfg = {
                         },
                         {
                             "include": "ows_refactored.elevation.ows_elevation_cfg.layer_cop_30",
+                            "type": "python",
+                        },
+                        {
+                            "include": "ows_refactored.elevation.ows_srtm_deriv_cfg.layer_srtm_deriv",
                             "type": "python",
                         },
                     ],
