96bcfc0d56a49575f6f13079e1196fad18c74bd5
Leon <106655586+lgkgh@users.noreply.github.com>
2022-08-18 13:13:43 +0200

    Add ndvi_anomaly to prod inventory (#428)
    * Add ndvi_anomaly to prod inventory
    
    * Add ndvi anom layer to prod ows root config
    
    * Updated NDVI layer description
    
    * Update NDVI styles in inventory
    
    * Remove whitespace
    
    Co-authored-by: Fang Yuan <fang.yuan@digitalearthafrica.org>

--- services/ows_refactored/vegetation/ows_ndvi_anom_cfg.py
+++ services/ows_refactored/vegetation/ows_ndvi_anom_cfg.py
@@ -2,7 +2,6 @@ from ows_refactored.common.ows_reslim_cfg import reslim_wms_min_zoom_15
 from ows_refactored.vegetation.styles_ndvi_anom import (
     style_ndvi_mean,
     style_ndvi_anomaly,
-    style_ndvi_anomaly_2std,
     style_count,
 )
 
@@ -10,14 +9,16 @@ bands_ndvi_anom = {"ndvi_std_anomaly": [], "ndvi_mean": [], "clear_count": []}
 
 
 layer = {
-    "title": "NDVI Anomaly (provisional)",
+    "title": "NDVI Anomaly",
     "name": "ndvi_anomaly",
     "abstract": """
-
 Standardised NDVI Anomalies provide a measure of vegetation health relative to long term average conditions by measuring the departure, in units of standard devaiations, away from the long-term mean. These monthly NDVI anomalies were calculated by combining NDVI observations from Landsat 8, Landsat 9, and Sentinel-2 for a given month, subtracting those values from the long term NDVI mean, then dividing by the long-term NDVI standard deviation. Postive values indicate vegetation is greener than average conditions, and are usually due to increased rainfall in a region. Negative values indicate additional plant stress relative to the long-term average.
 
-The long term means and standard deviations are available to load through the "NDVI Climatology" layer.
+This service provides data monthly from January 2017 at a spatial resolution of 30 m. The long term means and standard deviations used are available through the "NDVI Climatology" layer.
 
+More technical information about the NDVI Anomaly service can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/NDVI_Anomaly_specs.html)
+
+This service is accessible through OGC Web Service (https://ows.digitalearth.africa/), for analysis in DE Africa Sandbox JupyterLab (https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/wiki) and for direct download click on a tile in the explorer page (https://explorer.digitalearth.africa/products/ndvi_anomaly)
 """,
     "product_name": "ndvi_anomaly",
     "time_resolution": "month",
@@ -34,7 +35,6 @@ The long term means and standard deviations are available to load through the "N
         "default_style": "style_ndvi_anomaly",
         "styles": [
             style_ndvi_anomaly,
-            style_ndvi_anomaly_2std,
             style_ndvi_mean,
             style_count,
         ],
